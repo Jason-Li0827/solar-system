69edba76739bff63e638fe1861c187db789b2770
Jason-Li0827 <lijason0827@gmail.com>
2020-10-24 17:08:54 +0300

    Changed delay to speed
    Changed delay to speed
    And optimized planetSpeed

--- js_code.js
+++ js_code.js
@@ -1,52 +1,54 @@
 //changeable elements
-var canvasWidth = 1500; //don't forget to change the width and height value in <canvas>
-var canvasHeight = 1500;
-var a = 0;
+let canvasWidth = 1500; //don't forget to change the width and height value in <canvas>
+let canvasHeight = 1500;
+let minSpeed = 1;
+let maxSpeed = 100;
 
 //center of solor system
-var midX = 1 / 2 * canvasWidth;
-var midY = 1 / 2 * canvasHeight;
+let midX = 1 / 2 * canvasWidth;
+let midY = 1 / 2 * canvasHeight;
 
 //get canvas
-var ctx = myCanvas.getContext("2d");
+let ctx = myCanvas.getContext("2d");
 
 //get images
-var sun = new Image();
+let sun = new Image();
 sun.src = "sun.png";
 
-var mercury = new Image();
+let mercury = new Image();
 mercury.src = "mercury.png";
 
-var venus = new Image();
+let venus = new Image();
 venus.src = "venus.png";
 
-var earth = new Image();
+let earth = new Image();
 earth.src = "earth.png";
 
-var mars = new Image();
+let mars = new Image();
 mars.src = "mars.png";
 
-var jupiter = new Image();
+let jupiter = new Image();
 jupiter.src = "jupiter.png";
 
-var saturn = new Image();
+let saturn = new Image();
 saturn.src = "saturn.png";
 
-var uranus = new Image();
+let uranus = new Image();
 uranus.src = "uranus.png";
 
-var neptune = new Image();
+let neptune = new Image();
 neptune.src = "neptune.png";
 //This is the background image
-var background = new Image();
+let background = new Image();
 background.src = "sky.png";
 //Downloads the images and wait until it's done
 gaidaAtteluIeladi(done, sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, background);
 
 //setting variables
-var mercuryLoc = venusLoc = earthLoc = marsLoc = jupiterLoc = saturnLoc = uranusLoc = neptuneLoc = 0; //radians of angles
-//size of planets
-var planetR = {
+let mercuryLoc = venusLoc = earthLoc = marsLoc = jupiterLoc = saturnLoc = uranusLoc = neptuneLoc = 0; //radians of angles
+
+//size of planetSpeed
+let planetSize = {
     "sun": 80,
     "mercury": 36,
     "venus": 36,
@@ -57,8 +59,9 @@ var planetR = {
     "uranus": 72,
     "neptune": 72
 };
+
 //the distance between planet and the sun
-var planetD = {
+let planetDistance = {
     "mercury": 70,
     "venus": 120,
     "earth": 215,
@@ -68,59 +71,79 @@ var planetD = {
     "uranus": 600,
     "neptune": 700
 };
-//the speed of each planet
-//How I got those numbers:
-//1) s(the long numbers) = (2 * PI * r(distance to sun)) / time(for completing a revolution period, in days)
-//2) this number is the distance on its orbit it has to go by one turn
-//3) so to get the radians in for it, you have to divide this number by the planet's distance to sun(r)
-//4) because s / (2 * PI * r) = Δα / 2 * PI
-//5) Then you can get the final result
-//6*) This whole thing can be simplified as --> s = 2 * PI / time(for completing a revolution period, in days)
-var planetS = {
-    "mercury": 4.9996624038029086477133741318865 / planetD["mercury"], // 2D / ((430PI / 4.1) * (Revol.Per. / 365.26)) Ps. Revol.Per. in days
-    "venus": 3.3554317461731911709740147943125 / planetD["venus"],
-    "earth": 3.6984566947265399610848955832313 / planetD["earth"],
-    "mars": 2.7438230391295483645442838142824 / planetD["mars"],
-    "jupiter": 0.58007500123145474681078849925011 / planetD["jupiter"],
-    "saturn": 0.29198604345843261075723565702771 / planetD["saturn"],
-    "uranus": 0.12284233605978629394022125275566 / planetD["uranus"],
-    "neptune": 0.0730717089208274371119106320874 / planetD["neptune"],
-    //"neptune": 0.004819 //1 / 679.55
+
+//planetSpeed' revolution period(in days)
+//Ps. All the infos are from Wikipedia
+let daysOfPlanets = {
+    "mercury": 87.970534004061384,
+    "venus": 224.704984007334792,
+    "earth": 365.256363004,
+    "mars": 686.98147266518328,
+    "jupiter": 4332.670977953448,
+    "saturn": 10759.3932106451284,
+    "uranus": 30689.022247777582,
+    "neptune": 60190.59605942916
 };
 
-//console.log(2 * planetD["neptune"] / ((2 * planetD["earth"] * 4.1) * (60191.1954 / 365.26)));
+//the speed of each planet(in radians)
+//How does this works:
+//1) s = (2 * PI * r(distance to sun)) / time(for completing a revolution period, in days)
+//2) this number is the distance on its orbit it has to go by one turn
+//3) so to get the radians(Δα) in for it, you have to divide this number by the planet's distance to sun(r)
+//4) because s / (2 * PI * r) = Δα / 2 * PI
+//5) Then you can get the final result
+//6*) This whole thing can be simplified as --> Δα = 2 * PI / time(for completing a revolution period, in days)
+let planetSpeed = {
+    "mercury": 2 * Math.PI / daysOfPlanets["mercury"],
+    "venus": 2 * Math.PI / daysOfPlanets["venus"],
+    "earth": 2 * Math.PI / daysOfPlanets["earth"],
+    "mars": 2 * Math.PI / daysOfPlanets["mars"],
+    "jupiter": 2 * Math.PI / daysOfPlanets["jupiter"],
+    "saturn": 2 * Math.PI / daysOfPlanets["saturn"],
+    "uranus": 2 * Math.PI / daysOfPlanets["uranus"],
+    "neptune": 2 * Math.PI / daysOfPlanets["neptune"],
+};
+
 //basically does nothing
 //but gives a console log when all images all loaded
 function done() {
     console.log("Images loaded!")
 }
 
-//runs the main function once so that it draws the background image and the planets
+//runs the main function once so that it draws the background image and the planetSpeed
 //then use the pause function to stop it
 function setUp() {
     //This following line is needed because the variable "delay" should has a value before using it
     //Without this line, when you run this programme, the default "delay" is 1(I thinks so)
-    delay = document.getElementById("delay").value;
+    speed = document.getElementById("speed").value;
+    //The  slider is for speed changing, but the setTimeOut is using delay
+    //To change from speed to delay, you need this line of code
+    //This line is also used in some other place(delayCheckStart, delayCheckEnd, etc.).
+    delay = minSpeed + maxSpeed - speed;
     start();
     pause();
 }
 
 //Checks the change of delay while mouse is on slider
 function delayCheckStart() {
-    delay = document.getElementById("delay").value;
-    if (delay == 101)(
+    speed = document.getElementById("speed").value;
+    if (speed == 1) {
         delay = 500
-    );
+    } else {
+        delay = minSpeed + maxSpeed - speed;
+    }
     delayCheck = setTimeout(delayCheckStart, 10);
 }
 
 //Stops checking the change of delay
 //as the mouse left
 function delayCheckEnd() {
-    delay = document.getElementById("delay").value;
-    if (delay == 101)(
+    speed = document.getElementById("speed").value;
+    if (speed == 1) {
         delay = 500
-    );
+    } else {
+        delay = minSpeed + maxSpeed - speed;
+    }
     clearTimeout(delayCheck);
 }
 
@@ -142,8 +165,8 @@ function pause() {
 
 //calculate the middle of planet
 function getMid(imgName, x, y) {
-    var width = planetR[imgName];
-    var midP = {
+    let width = planetSize[imgName];
+    let midP = {
         "x": x - 1 / 2 * width,
         "y": y - 1 / 2 * width
     };
@@ -152,24 +175,24 @@ function getMid(imgName, x, y) {
 
 //draws the sun
 function drawSun() {
-    var middle = getMid("sun", midX, midY);
+    let middle = getMid("sun", midX, midY);
     ctx.drawImage(sun, middle["x"], middle["y"]);
 }
 
-//draws the planets
-function planets(planetImg, planetName, i) {
+//draws the planetSpeed
+function planetMovement(planetImg, planetName, i) {
     //get the distance between planet and sun(midpoint)
-    var r = planetD[planetName];
+    let r = planetDistance[planetName];
     //get the speed of revolutionary period
-    var s = planetS[planetName];
+    let s = planetSpeed[planetName];
     //The planet's orbit
     //Calculates the x and y coordinates as if the center is (0;0)
     //Then add the real midpoint's x and y value(x;y) to the result
     pointAngleInRadians = i;
-    var x = Math.cos(pointAngleInRadians) * r + midX;
-    var y = Math.sin(pointAngleInRadians) * r + midY;
+    let x = Math.cos(pointAngleInRadians) * r + midX;
+    let y = Math.sin(pointAngleInRadians) * r + midY;
     //get the midponi coordinate andn draw the planet
-    var middle = getMid(planetName, x, y);
+    let middle = getMid(planetName, x, y);
     //Draws the planet
     ctx.drawImage(planetImg, middle["x"], middle["y"]);
     //Add 1/8 radians to the planet's angle
@@ -177,8 +200,10 @@ function planets(planetImg, planetName, i) {
     //As 2Pi radians = 0 radians
     if (i >= 2 * Math.PI) {
         i -= 2 * Math.PI;
-    }
-
+    };
+    //This thing here check if the ratio is still right, using earth and neptune as samples
+    //The first result should be 1035..... something like that
+    //Don't forget to turn off the IF check above
     /*if (planetName == "neptune") {
         if (i >= 2 * Math.PI) {
             console.log(earthLoc)
@@ -198,14 +223,14 @@ function solarSystem() {
     ctx.drawImage(background, 0, 0);
     //draws the solar system
     drawSun();
-    mercuryLoc = planets(mercury, "mercury", mercuryLoc);
-    venusLoc = planets(venus, "venus", venusLoc);
-    earthLoc = planets(earth, "earth", earthLoc);
-    marsLoc = planets(mars, "mars", marsLoc);
-    jupiterLoc = planets(jupiter, "jupiter", jupiterLoc);
-    saturnLoc = planets(saturn, "saturn", saturnLoc);
-    uranusLoc = planets(uranus, "uranus", uranusLoc);
-    neptuneLoc = planets(neptune, "neptune", neptuneLoc);
+    mercuryLoc = planetMovement(mercury, "mercury", mercuryLoc);
+    venusLoc = planetMovement(venus, "venus", venusLoc);
+    earthLoc = planetMovement(earth, "earth", earthLoc);
+    marsLoc = planetMovement(mars, "mars", marsLoc);
+    jupiterLoc = planetMovement(jupiter, "jupiter", jupiterLoc);
+    saturnLoc = planetMovement(saturn, "saturn", saturnLoc);
+    uranusLoc = planetMovement(uranus, "uranus", uranusLoc);
+    neptuneLoc = planetMovement(neptune, "neptune", neptuneLoc);
     running = setTimeout(solarSystem, delay);
 }
 
@@ -219,16 +244,16 @@ function solarSystem() {
 // Autors:: 2018-2019 Ojārs Krūmiņš
 // Versija 1.3
 function gaidaAtteluIeladi() {
-    var images = [].slice.call(arguments);
-    var num = arguments.length;
-    var l = document.createElement('div');
+    let images = [].slice.call(arguments);
+    let num = arguments.length;
+    let l = document.createElement('div');
     l.id = "loading_div";
     l.innerHTML = "Attēlu ielāde ";
     document.body.appendChild(l);
-    var lcount = 1;
-    var interval = setInterval(function() {
-        var n = 1;
-        for (var i = 1; i < images.length; i++) {
+    let lcount = 1;
+    let interval = setInterval(function() {
+        let n = 1;
+        for (let i = 1; i < images.length; i++) {
             if (images[i].complete) {
                 n++;
             }
